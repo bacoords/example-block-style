b96cb7bff1bb319490469a4a32bfa8bd33cb18f0
Brian Coords <bacoords@gmail.com>
2023-06-16 11:02:04 -0700

    first commit

---
+++ example-block-style.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name:       Example Block Style
+ * Description:       Example block style tutorial.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       example-block-style
+ *
+ * @package           create-block
+ */
+
+function wp_dev_register_block_style() {
+
+	wp_enqueue_block_style(
+		'core/list',
+		array(
+			'handle' => 'example-block-style',
+			'src'    => plugins_url( 'example-block-style.css', __FILE__ ),
+		)
+	);
+
+	register_block_style(
+		'core/list',
+		array(
+			'name'  => 'check', // .is-style-check
+			'label' => 'Check',
+		)
+	);
+}
+add_action( 'init', 'wp_dev_register_block_style' );
---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Example Block Style ===
+Contributors:      The WordPress Contributors
+Tags:              block
+Tested up to:      6.1
+Stable tag:        0.1.0
+License:           GPL-2.0-or-later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+
+Example block style.
